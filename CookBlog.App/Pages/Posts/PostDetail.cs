c4b2091ea4f5c0d27b7c7a431c7b85343beecb59
Grzegorz Bączek <Gregor333000@gmail.com>
2023-09-25 10:11:37 +0200

    good choice

--- CookBlog.App/Pages/Posts/PostDetail.cs
+++ CookBlog.App/Pages/Posts/PostDetail.cs
@@ -1,38 +1,37 @@
-﻿using CookBlog.App.DTO;
-using CookBlog.App.Services;
-using Microsoft.AspNetCore.Components;
+﻿//using CookBlog.App.DTO;
+//using CookBlog.App.Services;
+//using Microsoft.AspNetCore.Components;
 
-namespace CookBlog.App.Pages.Posts;
+//namespace CookBlog.App.Pages.Posts;
 
-public partial class PostDetail
-{
-    [Parameter]
-    public string PostId { get; set; }
-    public PostDto PostDto { get; set; } = new PostDto();
-    public CommentDto CommentDto { get; set; } = new CommentDto();
-    public CreateCommentDto CreateCommentDto { get; set; } = new CreateCommentDto();
+//public partial class PostDetail
+//{
+//    [Parameter]
+//    public string PostId { get; set; }
+//    public PostDto PostDto { get; set; } = new PostDto();
+//    public CommentDto CommentDto { get; set; } = new CommentDto();
+//    public CreateCommentDto CreateCommentDto { get; set; } = new CreateCommentDto();
 
-    [Inject]
-    public IPostDataService PostDataService { get; set; }
-    [Inject]
-    public ICommentDataService CommentDataService { get; set; }
+//    [Inject]
+//    public IPostDataService PostDataService { get; set; }
+//    [Inject]
+//    public ICommentDataService CommentDataService { get; set; }
 
-    protected override async Task OnInitializedAsync()
-    {
-        PostDto = await PostDataService.GetPostAsync(Guid.Parse(PostId));
+//    //protected override async Task OnInitializedAsync()
+//    //{
+//    //    PostDto = await PostDataService.GetPostAsync(Guid.Parse(PostId));
+//    //}
 
-    }
+//    public async Task AddComment()
+//    {
+//        var createCommentDto = new CreateCommentDto
+//        {
+//            PostId = Guid.Parse(PostId),
+//            FullName = CommentDto.FullName,
+//            Description = CommentDto.Description
+//        };
+//        await CommentDataService.AddCommentAsync(createCommentDto);
 
-    public async Task AddComment()
-    {
-        var createCommentDto = new CreateCommentDto
-        {
-            PostId = Guid.Parse(PostId),
-            FullName = CommentDto.FullName,
-            Description = CommentDto.Description
-        };
-        await CommentDataService.AddCommentAsync(createCommentDto);
-
-        CommentDto = new CommentDto();
-    }
-}
+//        CommentDto = new CommentDto();
+//    }
+//}
